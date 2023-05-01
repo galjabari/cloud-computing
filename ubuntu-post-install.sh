e64932295632e3cca59db9455118c9616d67bdd4
galjabari <72796602+galjabari@users.noreply.github.com>
2023-05-01 22:07:24 +0300

    Delete ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install open-vm-tools-desktop -y
-sudo apt-get install git -y
-git clone https://github.com/galjabari/cloud-computing.git
-bash cloud-computing/install-docker.sh
-bash cloud-computing/install-portainer.sh
-bash cloud-computing/install-vscode.sh
