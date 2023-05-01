0be641bb88060c63982eed59a1766aa683f9b4a0
galjabari <72796602+galjabari@users.noreply.github.com>
2023-05-01 21:38:12 +0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install open-vm-tools-desktop -y
 sudo apt-get install git -y
 git clone https://github.com/galjabari/cloud-computing.git
-sh cloud-computing/install-docker.sh
-sh cloud-computing/install-portainer.sh
-sh cloud-computing/install-vscode.sh
+bash cloud-computing/install-docker.sh
+bash cloud-computing/install-portainer.sh
+bash cloud-computing/install-vscode.sh
