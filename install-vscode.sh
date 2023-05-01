9b5ff9b3130d6c91c38aa5f3f9edbdf011ff326d
galjabari <72796602+galjabari@users.noreply.github.com>
2023-05-01 13:08:27 +0300

    Update install-vscode.sh

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,10 +1,10 @@
-# Setup
-sudo apt-get install wget gpg
+## Setup
+sudo apt-get install wget gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > p>
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings>
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packa>
 rm -f packages.microsoft.gpg
 ## Install
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y
 sudo apt update
-sudo apt install code # or code-insiders
+sudo apt install code -y # or code-insiders
